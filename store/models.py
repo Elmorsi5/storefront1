31adf8baf17195b6eb64e20dd0eca90541fac398
Elmorsi5 <mohamedelmorse5@gmail.com>
2023-08-30 02:07:02 +0300

    Order Model and some updates

--- store/models.py
+++ store/models.py
@@ -9,8 +9,23 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    class MEMBERSHIP_CHOICES(models.TextChoices):
+        BRONZE = 'B','Bronze'
+        SILVER = 'C','Silver'
+        GOLD = 'F','Gold'
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique= True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null = True)
+    membership = models.models.CharField(max_length=1,choices =MEMBERSHIP_CHOICES.choices,default = MEMBERSHIP_CHOICES.BRONZE )
+
+
+class Order(models.Model):
+    class PAYMENT_STATUS(models.TextChoices):
+        PENDING = 'P','Pending'
+        COMPLETE = 'C','Complete'
+        FAILED = 'F','Failed'
+
+    placed_at = models.DateTimeField(auto_now_add= True)
+    payment_status = models.CharField(max_length=1,choices=PAYMENT_STATUS.choices,default=PAYMENT_STATUS.PENDING)
