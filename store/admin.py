5aba437110d5677e3c3780256ed62fef3960ac09
Elmoris5 <mohamedelmorse5@gmail.com>
2023-10-01 12:04:49 +0300

    Provide links to collection admin panel to view products of each collection

--- store/admin.py
+++ store/admin.py
@@ -3,8 +3,11 @@ from typing import Any
 from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
+from django.urls import reverse
 from .models import Collection,Product,Customer,Order
 from django.db.models import Count
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
 # The original way to add model in the admin site and then applay it's customization which in it's ModelAdmin
 # class ProductAdmin(admin.ModelAdmin):
@@ -12,6 +15,7 @@ from django.db.models import Count
 # admin.site.register(Product,ProductAdmin)
 
 # Register your models here.
+
 
 @admin.register(Product)    # here we are saying that Productadmin is the AdminModel of the Product model - register func take the model to register and applay what in it's modeladmin
 class ProductAdmin(admin.ModelAdmin):
@@ -31,19 +35,26 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'Ok'
     
+
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ['title','products_count']
+    list_display = ['id','title','products_count']
 
     @admin.display(ordering='products_count')
     def products_count(self,collectoin):
-        return collectoin.products_count
+        url = (reverse('admin:store_product_changelist')
+                + '?'
+                + urlencode({
+                    'collection__id' : str(collectoin.id)
+                }))
+        return format_html('<a href="{}">{}</a>',url,collectoin.products_count)
+
+    
     # for each collection object give me the count of the related procuts
     def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
         return super().get_queryset(request).annotate(
             products_count = Count('product')
         )
-
 
 
 @admin.register(Customer)
@@ -53,10 +64,6 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 10
     ordering = ['first_name','last_name']
 
-    @admin.register(Order)
-    class OrderAdmin(admin.ModelAdmin):
-        list_display = ['id','placed_at','payment_status','customer']
-    
     #override the base queryset using annotate to return the number of orders for each customer
     @admin.display(ordering='orders_count')
     def orders_count(self,customer):
@@ -66,6 +73,11 @@ class CustomerAdmin(admin.ModelAdmin):
         return super().get_queryset(request).annotate(
             orders_count = Count('order')
         )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','placed_at','payment_status','customer']
 
 
 
