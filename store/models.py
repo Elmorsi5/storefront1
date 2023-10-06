be294b9b2ae695f7de08e5d0112ddce6bddfb55b
Elmoris5 <mohamedelmorse5@gmail.com>
2023-10-06 16:43:01 +0300

    Create Review data model

--- store/models.py
+++ store/models.py
@@ -108,3 +108,9 @@ class CartItem(models.Model):
 
     def __str__(self) -> str:
         return str(self.product)
+
+class Review(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='reviews')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
