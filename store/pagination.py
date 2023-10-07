12daf6b27904a52b68510478dd5a232e6265b5c4
Elmoris5 <mohamedelmorse5@gmail.com>
2023-10-07 19:11:00 +0300

    Create Customer Pagination

--- store/pagination.py
+++ store/pagination.py
@@ -1,4 +1,8 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination,LimitOffsetPagination
 
 class ProductPagination(PageNumberPagination):
+    page_size = 10
+
+
+class CustomerPagination(PageNumberPagination):
     page_size = 10
